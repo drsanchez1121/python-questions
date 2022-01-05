b07ad55940b896d4bf527258beac2bb1fa47a783
drsanchez1121 <drsanchez1121@gmail.com>
2022-01-05 12:28:38 -0600

    Add files via upload

---
+++ beginner_exercises.py
@@ -0,0 +1,23 @@
+# Use for the questions below:
+nums = [i for i in range(1,1001)]
+string = "Practice Problems to Drill List Comprehension in Your Head."
+
+# Questions 1–5 will be relatively straightforward, while questions 6–8 will be a little more advanced.
+# Place your answers in a separate file
+
+
+# 1. Find all of the numbers from 1–1000 that are divisible by 8
+
+# 2. Find all of the numbers from 1–1000 that have a 6 in them
+
+# 3. Count the number of spaces in a string (use string above)
+
+# 4. Remove all of the vowels in a string (use string above)
+
+# 5. Find all of the words in a string that are less than 5 letters (use string above)
+
+# 6. Use a dictionary comprehension to count the length of each word in a sentence (use string above)
+
+# 7. Use a nested list comprehension to find all of the numbers from 1–1000 that are divisible by any single digit besides 1 (2–9)
+
+# 8. For all the numbers 1–1000, use a nested list/dictionary comprehension to find the highest single digit any of the numbers is divisible by
